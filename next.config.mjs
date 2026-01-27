a38d81bb724495dfdea4b59801ebe6f2e9492a9f
Italo <italo520@gmail.com>
2026-01-26 21:36:50 -0300

    feat: migrate PWA to @ducanh2912/next-pwa and update build config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
-/** @type {import('next').NextConfig} */
-import withPWAInit from 'next-pwa';
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig = {
